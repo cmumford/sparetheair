afbd7dfc7e9c2310d61e671765b3fa765cc767b7
Chris Mumford <cmumford@cmumford.com>
2019-12-01 14:39:24 -0800

    Using new fonts.

--- sparetheair/display.cpp
+++ sparetheair/display.cpp
@@ -131,6 +131,7 @@ void Display::DrawString(const String& str, const Point& pt, uint16_t color) {
 }
 
 void Display::DrawTodayEntry(const Status& status) {
+  display_.setFont(&windows_command_prompt16pt7b);
   if (kDrawBorders) {
     display_.drawRect(kTodayBounds.left(), kTodayBounds.top(),
                       kTodayBounds.width(), kTodayBounds.height(), kRedColor);
@@ -162,11 +163,13 @@ void Display::DrawForecast(const Status& status, const Rectangle& bounds) {
     display_.drawRect(bounds.left(), bounds.top(), bounds.width(),
                       bounds.height(), kRedColor);
   }
+  display_.setFont(&windows_command_prompt16pt7b);
   const int kMargin = 4;
   DrawString(GetDayOfWeekAbbrev(status.day_of_week),
              Point({bounds.left() + kMargin, bounds.top() + kMargin}),
              kBlackColor);
 
+  display_.setFont(&LibreBaskerville_Bold28pt7b);
   const int kLineHeight = 14;
   String aqi_str = status.aqi_val != -1
                        ? String(status.aqi_val)
